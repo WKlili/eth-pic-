39922b031e645cb4a34ba6ee694ad59304868c9d
nemo <18136225579@163.com>
2021-05-15 21:56:53 +0800

    faeture: set fleek

--- src/components/Upload/Upload.tsx
+++ src/components/Upload/Upload.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import fleekStorage from '@fleekhq/fleek-storage-js';
 import cn from 'classnames';
 
 import styles from './Upload.module.css';
@@ -18,9 +19,26 @@ export function Upload(props: UploadProps) {
       />
       <p className={styles.description}>Upload photos</p>
 
-      <div className={styles.uploadBtn}>Select photos</div>
+      <div className={styles.uploadBtn}>
+        Select Photos
+        <input
+          onChange={uploadFile}
+          className={styles.uploadFile}
+          type="file"
+        />
+      </div>
 
       <div className={styles.warning}></div>
     </div>
   );
+
+  async function uploadFile(e: any) {
+    const uploadedFile = await fleekStorage.upload({
+      apiKey: 'O8kkDM1CU2Ry8mLw0neBHA==',
+      apiSecret: '4z1E20GyGN6X0pdywh6jGW4ZVLa+F8J0gVwgOH+2H6s=',
+      key: 'ceshi',
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      data: e.target.files[0] as string
+    });
+  }
 }
